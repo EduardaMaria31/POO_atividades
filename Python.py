cd0d46bd9ff83d7c6a4696365c1442c6f9a88486
Maria Eduarda <eduardamaria.wm31@gmail.com>
2025-10-21 00:09:55 -0300

    Atividade POO- 20/10/2025 - Ads 2°B
    Atividade POO- 20/10/2025 - Ads 2°B

---
+++ Python.py
@@ -0,0 +1,26 @@
+# 1°questão
+
+numeros = list((7, 8 , 9 ,6 ,2 ))
+
+# 2°questão
+
+print(numeros [0])
+print(numeros[4])
+print(numeros[2])
+
+# 3°questão
+
+numeros[4] = 4
+print(numeros)
+
+# 4°questão
+
+num = int(input("Digite um número:"))
+numeros.append(num)
+print(numeros)
+
+# 5°questão
+
+numeros.pop(1)
+print(numeros)
+
